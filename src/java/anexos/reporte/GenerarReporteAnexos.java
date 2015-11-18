86e12860db74b0a005bd1c3658b36e4ae08309a6
caguachisaca@gmail.com <caguachisaca@gmail.com>
2015-11-18 11:39:11 -0500

    modificacion en retenciones en venta

--- src/java/anexos/reporte/GenerarReporteAnexos.java
+++ src/java/anexos/reporte/GenerarReporteAnexos.java
@@ -41,7 +41,7 @@ public class GenerarReporteAnexos implements GenerarReporteAnexosLocal {
         try {
 
             String rutaReportes = getRutaReportes() + "anexos/";
-            System.out.println("ruta anexos: "+rutaReportes+nombreReporte);
+            System.out.println("ruta anexos: " + rutaReportes + nombreReporte);
             parametros.put("SUBREPORT_DIR", rutaReportes);
             parametros.put("p_empresa_nombre", sisUsuario.getEmpNombre());
             parametros.put("p_empresa_ruc", sisUsuario.getEmpRuc());
@@ -81,6 +81,7 @@ public class GenerarReporteAnexos implements GenerarReporteAnexosLocal {
             return null;
         }
     }
+
     public JasperPrint generarReporteTalonResumenVentas(SisUsuarioEmpresaTO sisUsuarioEmpresaTO,
             String desde, String hasta, List<AnxTalonResumenVentaTO> listaAnxTalonResumenVentaTO) throws Exception {
         try {
@@ -115,14 +116,14 @@ public class GenerarReporteAnexos implements GenerarReporteAnexosLocal {
             for (AnxListaRetencionesTO plgTO : listaAnxListaRetencionesTO) {
                 ReporteAnxListaRetenciones reporteAnxListaRetencion = new ReporteAnxListaRetenciones();
 
-<<<<<<< HEAD
+
                 reporteAnxListaRetencion.setRetProveedorId((plgTO.getRetProveedorId() == null ? "" : plgTO.getRetProveedorId()));
-=======
+
 //                reporteAnxListaRetencion.setRetSustentoTributario((plgTO.getRetSustentoTributario() == null ? "" : plgTO.getRetSustentoTributario()));
                 reporteAnxListaRetencion.setRetProveedorId(plgTO.getRetProveedorId());
-                System.out.println("id: "+plgTO.getRetProveedorId());
-                System.out.println("nombre prov.: "+plgTO.getRetProveedorNombre());
->>>>>>> 88d78299ace679738af18824534caf26ed995496
+                System.out.println("id: " + plgTO.getRetProveedorId());
+                System.out.println("nombre prov.: " + plgTO.getRetProveedorNombre());
+
                 reporteAnxListaRetencion.setRetProveedorNombre((plgTO.getRetProveedorNombre() == null ? "" : plgTO.getRetProveedorNombre()));
                 reporteAnxListaRetencion.setRetDocumentoNombre((plgTO.getRetDocumentoNombre() == null ? "" : plgTO.getRetDocumentoNombre()));
                 reporteAnxListaRetencion.setRetDocumentoNumero((plgTO.getRetDocumentoNumero() == null ? "" : plgTO.getRetDocumentoNumero()));
