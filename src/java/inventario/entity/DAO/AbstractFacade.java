d884a25224f76957e551a9bddb10984ed7a6672e
Carlos Ajila <carlos.ajila.m@gmail.com>
2015-11-24 16:33:45 -0500

    actualizar AbstractFacade de inventario

--- src/java/inventario/entity/DAO/AbstractFacade.java
+++ src/java/inventario/entity/DAO/AbstractFacade.java
@@ -1,62 +1,55 @@
+
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 package inventario.entity.DAO;
 
 import java.util.List;
 import javax.persistence.EntityManager;
 
 /**
- *
- * @author developer1
- */
+*
+* @author developer1
+*/
 public abstract class AbstractFacade<T> {
-    private Class<T> entityClass;
+   private Class<T> entityClass;
 
-    public AbstractFacade(Class<T> entityClass) {
-        this.entityClass = entityClass;
-    }
+   public AbstractFacade(Class<T> entityClass) {
+       this.entityClass = entityClass;
+   }
 
-    protected abstract EntityManager getEntityManager();
+   protected abstract EntityManager getEntityManager();
 
-    public void create(T entity) {
-        getEntityManager().persist(entity);
-    }
+   public void create(T entity) {
+       getEntityManager().persist(entity);
+   }
 
-    public void edit(T entity) {
-        getEntityManager().merge(entity);
-    }
+   public void edit(T entity) {
+       getEntityManager().merge(entity);
+   }
 
-    public void remove(T entity) {
-        getEntityManager().remove(getEntityManager().merge(entity));
-    }
+   public void remove(T entity) {
+       getEntityManager().remove(getEntityManager().merge(entity));
+   }
 
-    public T find(Object id) {
-        return getEntityManager().find(entityClass, id);
-    }
+   public T find(Object id) {
+       return getEntityManager().find(entityClass, id);
+   }
 
-    public List<T> findAll() {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
-        return getEntityManager().createQuery(cq).getResultList();
-    }
+   public List<T> findAll() {
+       return getEntityManager().createQuery("select object(o) from " + entityClass.getSimpleName() + " as o").getResultList();
+   }
 
-    public List<T> findRange(int[] range) {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
-        javax.persistence.Query q = getEntityManager().createQuery(cq);
-        q.setMaxResults(range[1] - range[0]);
-        q.setFirstResult(range[0]);
-        return q.getResultList();
-    }
+   public List<T> findRange(int[] range) {
+       javax.persistence.Query q = getEntityManager().createQuery("select object(o) from " + entityClass.getSimpleName() + " as o");
+       q.setMaxResults(range[1] - range[0]);
+       q.setFirstResult(range[0]);
+       return q.getResultList();
+   }
 
-    public int count() {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
-        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-        javax.persistence.Query q = getEntityManager().createQuery(cq);
-        return ((Long) q.getSingleResult()).intValue();
-    }
-    
+   public int count() {
+       return ((Long) getEntityManager().createQuery("select count(o) from " + entityClass.getSimpleName() + " as o").getSingleResult()).intValue();
+   }
+   
 }
